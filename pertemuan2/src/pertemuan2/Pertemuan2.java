702b6ccf3161cffcf31e11561f0ea34ac7b64040
Ihfald <rivaldushjelatu@gmail.com>
2025-03-11 21:57:35 +0800

    pertemuan2

---
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,25 @@
+package pertemuan2;
+
+/**
+ *
+ * @author Ihfald
+ * tgl:11 Maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama = "Ihfald "; 
+       String nim = "2301010495";
+       
+       System.out.println ("mari pulang");
+       System.out.println ("-------------------");
+       System.out.println ("Nama : "+nama);
+       System.out.println ("---------------");
+       System.out.println("NIM :"+nim);
+       System.out.printf ("Nama saya %s dengan NIM %s,saat ini sedang belajar bahasa java",nama,nim);
+    }
+    
+}
